056d4da3f17e36a48a28576ddbcd48763ee9854f
Fazal013 <khajafazalkhan@gmail.com>
2024-08-31 00:27:26 +0530

    Add project files

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
-        secret:process.env.SECRET,
+        secret: process.env.SECRET,
     },
     touchAfter:24 * 3600,
 });
